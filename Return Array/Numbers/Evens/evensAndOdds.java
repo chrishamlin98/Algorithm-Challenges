9e3b33fa7ce962f4a3024826c7d6e627eee702b3
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-15 10:44:41 -0700

    Update evensAndOdds.java

--- Return Array/Numbers/Evens/evensAndOdds.java
+++ Return Array/Numbers/Evens/evensAndOdds.java
@@ -1,16 +1,26 @@
-class Solution {
-    public int[] sortArrayByParity(int[] A) {
-        int[] ans = new int[A.length];
-        int t = 0;
+import java.util.Arrays;
 
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 0)
-                ans[t++] = A[i];
+public class ReturnEvenAndOdds2 {
 
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 1)
-                ans[t++] = A[i];
+		public static void main(String[] args) {
+			//instantiate array and fill with numbers
+			int[] nums = {1, 2, 3, 4, 5};
+			System.out.println(Arrays.toString(ReturnEvenAndOdds2.sortArrayByParity(nums)));
 
-        return ans;
-    }
-}
+		}
+
+	    public static int[] sortArrayByParity(int[] A) {
+	        int[] array = new int[A.length];
+	        int t = 0;
+
+	        for(int i = 0; i < A.length; i++) {
+	            if (A[i] % 2 == 0)
+	                array[t++] = A[i];}
+
+	        for (int i = 0; i < A.length; i++) {
+	            if (A[i] % 2 == 1)
+	                array[t++] = A[i];
+	        }
+	        return array;
+	    }
+	}

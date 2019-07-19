cbad0a318e935ef40e246ca7be0b92dd1aeab0a7
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-18 18:13:44 -0700

    Update ArrayIndexDeletion_Incomplete.java

--- Move 0's/ArrayIndexDeletion_Incomplete.java
+++ Move 0's/ArrayIndexDeletion_Incomplete.java
@@ -6,7 +6,11 @@ class Solution {
         int[] array = new int[5];
         for (i = 0; i < array.length; i++){
             if (array[i] == 0) {
-                int removeIndex = i;
+             
+/* ShrinedToday at 6:07 PM
+@chamlin I think starting off, you should loop through the array finding any non-zero integers. use one variable  (we'll call it counter) that only increases everytime a new element is found, and the variable in your for loop, in this case "i", should iterate every check and when a non-zero is found you set arr[counter] = arr[i]. After that is traversed, you could use another for loop that starts at where the counter variable ended, and adds 0's until it reaches the end of the array
+time complexity is O(N*2) or O(N), there might be a more optimal solution... then later, you're replacing the number of 0's there was originally */             
+             
             }
         }
         return;

fc455108a205f29047b3c113bf1e31de3710284a
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-18 17:45:08 -0700

    Create ArrayIndexDeletion_Incomplete.java

---
+++ Move 0's/ArrayIndexDeletion_Incomplete.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i;
+        int[] array = new int[5];
+        for (i = 0; i < array.length; i++){
+            if (array[i] == 0) {
+                int removeIndex = i;
+            }
+        }
+        return;
+        }
+            
+    }

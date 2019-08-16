cc3e19533a9020fde28b4079d05d79c505229a0a
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-08-16 06:44:31 -0700

    Create Upper to Lowercase.java

---
+++ Character transformations/Upper to Lowercase.java
@@ -0,0 +1,25 @@
+/* Implement function ToLowerCase() that has a string
+ parameter str, and returns the same string in lowercase.
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+*/
+
+public static void BigToLittle(char little []) {
+  little [] =
+  // I need to find the code that changes capital letters to lowercase
+}
+
+public static void main(String []args) {
+
+}

9ec8161b8873ec246f1e4309e687c0e0fa7d8024
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-09-03 18:14:01 -0700

    Create defang.java

---
+++ Defang an IP/defang.java
@@ -0,0 +1,45 @@
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+
+Constraints:
+
+The given address is a valid IPv4 address.
+
+*/
+
+/* This solution would require StringBuilder type and I
+do not have enough familiarity with this.  This would be more efficient
+build...
+
+class Solution {
+    public String defangIPaddr(String address) {
+        String[] defangIPaddr;
+        for(int i = 0, i < address.length(); i++)
+          {
+          if(".")
+          {
+            System.out.println("[.]");
+          }
+        }
+        return Solution;
+    }
+}
+*/
+
+class Solution {
+    public String defangIPaddr(String address) {
+       return address.replace(".", "[.]");
+    }
+}

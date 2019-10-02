04398093555eb0788105003222dd8e37fe386e42
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-10-02 15:08:53 -0700

    Update Islands.java

---
+++ LC. 200 - Number of Islands/Islands.java
@@ -0,0 +1,23 @@
+int count = 0;
+
+for (int i = 0; i < grid.length; i++){
+  for int j = 0; j < grid[i].length; j++){
+    if (grid [i][j] == '1'){
+      count +- 1;
+      search (grid, i, j);
+    }
+  }
+}
+
+return count;
+
+  public void search (char[][] grid, int i, int j){
+    if (i < 0 || i >= grid.length || j  < 0 || j > grid[i].length || grid [i][j] == '0')
+    return;
+
+    grid [i][j] = '0';
+    search (grid, i+1, j);
+    search (grid, i-1, j);
+    search (grid, i, j+1);
+    search (grid, i, j-1);
+  }

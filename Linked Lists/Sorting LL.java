ac483d59e5f01a427637892a5a9743c7e8bd2eb7
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-10-06 06:32:18 -0700

    Create Sorting LL.java

---
+++ Linked Lists/Sorting LL.java
@@ -0,0 +1,14 @@
+package linkedLists;
+
+public class MergeLinkedList {
+
+	if(head = null || head <= 0) {
+		return;
+	} else {
+	}
+	public static void main(String[] args) {
+		int head;
+
+	}
+
+}

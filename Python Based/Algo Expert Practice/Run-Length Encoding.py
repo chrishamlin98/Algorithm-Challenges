52a8cbd15f31e8b35d2e1d379413c87c22b716ff
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-01-12 06:26:14 -0700

    Adding a new problem to solve - run-length encoding

---
+++ Python Based/Algo Expert Practice/Run-Length Encoding.py
@@ -0,0 +1,7 @@
+# Write a function that takes in a non-empty string and returns its run-length encoding.
+# Basically, take a run of data and compress it "AAAAAAA" = "7A"
+# Any string length longer than 9 must be split - ie
+# "AAAAAAAAAAAAA" = "9A4A"
+
+def runLengthEncoding(string):
+    pass

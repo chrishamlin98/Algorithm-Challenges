0e9ee234e1c4a135914967a978d87411bbc48e44
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-01-14 06:02:22 -0700

    completed run length

--- Python Based/Algo Expert Practice/Run-Length Encoding.py
+++ Python Based/Algo Expert Practice/Run-Length Encoding.py
@@ -6,4 +6,21 @@
 # Coded in notebook - complete another day
 
 def runLengthEncoding(string):
+    characters = []
+    length = 1
+
+    for i in range(1, len(string)):
+        current = string[i]
+        prev = string[i-1]
+
+        if current != prev or length == 9:
+            characters.append(str(length))
+            characters.append(prev)
+            length = 0
+            length += 1
+
+    characters.append(str(length))
+    characters.append(string[len(string) - 1])
+
+    return "".join(characters)
     pass

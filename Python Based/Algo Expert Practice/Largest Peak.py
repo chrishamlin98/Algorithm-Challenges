e28f3c0283de29c2144d47978184714d7a387dc1
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-02-04 07:02:29 -0700

    Still working, does not pass all test cases

--- Python Based/Algo Expert Practice/Largest Peak.py
+++ Python Based/Algo Expert Practice/Largest Peak.py
@@ -1,5 +1,35 @@
 # I think we need to separate the left and right peakcheckers in order to keep them increasing or decreasing
 
+# I think we need to separate the left and right peakcheckers in order to keep them increasing or decreasing
+# After 2nd day of reviewing, it seems we will be traversing the array and finding peaks then
+# moving left and right to find the length and store that value.  Then, find another and compare
+## This passes some test cases but not all - code under neath does not work
+def longestPeak(array):
+    longestPeak = 0
+    i = 1
+    while i < len(array) - 1:
+        peak = array[i - 1] > array[i] > array[i + 1]
+        if not peak:
+            i += 1
+            continue
+
+        leftIdx = i - 2
+        rightIdx = i + 2
+        while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
+            leftIdx -= 1
+        while rightIdx < len(array) and array[rightIdx] < array[rightIdx - 1]:
+            rightIdx += 1
+
+        currentPeakLength = rightIdx - leftIdx - 1
+        longestPeak = max(currentPeakLength, longestPeak)
+        i = rightIdx
+
+    return longestPeak
+
+
+pass
+
+
 def longestPeak(array):
     peakChecker = 1
     currentPeak = 0

3000db71b88b39c289ea7cb17dfd3fc2cf459598
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-02-06 06:29:20 -0700

    Fixed direction of greater than less than sign in logic in order to fix

--- Python Based/Algo Expert Practice/Largest Peak.py
+++ Python Based/Algo Expert Practice/Largest Peak.py
@@ -1,14 +1,11 @@
 # I think we need to separate the left and right peakcheckers in order to keep them increasing or decreasing
-
-# I think we need to separate the left and right peakcheckers in order to keep them increasing or decreasing
 # After 2nd day of reviewing, it seems we will be traversing the array and finding peaks then
 # moving left and right to find the length and store that value.  Then, find another and compare
-## This passes some test cases but not all - code under neath does not work
 def longestPeak(array):
     longestPeak = 0
     i = 1
     while i < len(array) - 1:
-        peak = array[i - 1] > array[i] > array[i + 1]
+        peak = array[i - 1] < array[i] > array[i + 1]
         if not peak:
             i += 1
             continue
@@ -21,27 +18,10 @@ def longestPeak(array):
             rightIdx += 1
 
         currentPeakLength = rightIdx - leftIdx - 1
-        longestPeak = max(currentPeakLength, longestPeak)
+        longestPeak = max(longestPeak, currentPeakLength)
         i = rightIdx
 
     return longestPeak
-
-
-pass
-
-
-def longestPeak(array):
-    peakChecker = 1
-    currentPeak = 0
-    largestPeak = 0
-
-    for peakChecker in array:
-        while array[peakChecker] - 1 < array[peakChecker]:
-            peakChecker += 1
-            currentPeak += 1
-        while array[peakChecker] < array[peakChecker] + 1:
-            peakChecker += 1
-            currentPeak += 1
 
 
 pass

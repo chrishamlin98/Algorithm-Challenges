455fe66e04997064798a3d092d71fff5700ab014
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-02-07 12:16:36 -0700

    Array of Products - Brute Force + Optimal

---
+++ Python Based/Algo Expert Practice/Array of Products.py
@@ -0,0 +1,35 @@
+# Brute Force
+#
+# def arrayOfProducts(array):
+#     products = [1 for _ in range(len(array))]
+#
+# 	for i in range(len(array)):
+# 		runningProduct = 1
+# 		for j in range(len(array)):
+# 			if i != j:
+# 				runningProduct *= array[j]
+# 		products[i] = runningProduct
+# 	return products
+#     pass
+
+# optimal solution  Time = O(N)  Space = O(N)
+
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        leftProducts[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        rightProducts[i] = rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
+
+    return products

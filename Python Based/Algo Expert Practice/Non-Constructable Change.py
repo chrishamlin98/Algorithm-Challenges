6a3485bf828adeafcaafa435bd85d076b6d45f8e
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-02-18 06:56:41 -0700

    Non-constructable change explanation - no code yet

---
+++ Python Based/Algo Expert Practice/Non-Constructable Change.py
@@ -0,0 +1,8 @@
+# Given an array of change values, write a function that returns
+# the smallest amount that you cannot make
+
+#  Brute force solution is too complicated because it would require
+# multiple loops and complicated math
+
+# Instead, we should sort the list and then use math understanding
+# to come up with the solution

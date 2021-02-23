3e3af20d76a05262fd49af50893a6fde2a1c6b7d
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-02-23 06:40:58 -0700

    Added code to return non-constructable change

--- Python Based/Algo Expert Practice/Non-Constructable Change.py
+++ Python Based/Algo Expert Practice/Non-Constructable Change.py
@@ -6,3 +6,14 @@
 
 # Instead, we should sort the list and then use math understanding
 # to come up with the solution
+
+def nonConstructableChange(coins):
+    coins.sort()
+    change = 0
+
+    for i in coins:
+        if i > change + 1:
+            return change + 1
+        change += i
+
+    return change + 1

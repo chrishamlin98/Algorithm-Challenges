dcf3822e7c74ba6ce4ccbc4706329ee7af8b515e
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-03-13 05:11:34 -0700

    Invert Binary Tree

---
+++ Python Based/Algo Expert Practice/Invert Binary Tree.py
@@ -0,0 +1,21 @@
+# This function allows the levels of the tree to basically swap their children and then
+# themselves.  The alternate algorithm requires stacks and ques and popping values.
+
+def invertBinaryTree(tree):
+    if tree is None:
+        return
+    swapLeftAndRight(tree)
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None

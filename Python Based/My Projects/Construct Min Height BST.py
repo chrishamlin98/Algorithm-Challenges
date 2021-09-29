b83d67cbde1064405a8ca4d4a0f8908224d73613
chrishamlin98@gmail.com <chrishamlin98@gmail.com>
2021-09-29 08:22:41 -0700

    BST - min height best time/space

---
+++ Python Based/My Projects/Construct Min Height BST.py
@@ -0,0 +1,31 @@
+def minHeightBst(array):
+    return constructBst(array, 0, len(array) - 1)
+
+# This solution does not use the provided insert method
+# Time/Space = O(n)
+def constructBst(array, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    bst = BST(array[midIdx])
+    bst.left = constructBst(array, startIdx, midIdx-1)
+    bst.right = constructBst(array, midIdx + 1, endIdx)
+    return bst
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
